3cf75d32d129867dc93501c19b10d1226af67454
Vishnu Tejas Tadipatri <vtadipatri28@gmail.com>
2026-06-09 22:29:35 -0400

    Allow gitlab.igem.org scripts in CSP and add global CSP header

--- api/raw.js
+++ api/raw.js
@@ -24,6 +24,6 @@ export default function handler(req, res) {
   const contentType = safePath.endsWith('.css') ? 'text/css' : 'text/html';
   res.setHeader('Content-Type', contentType);
   res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.setHeader('Content-Security-Policy', "script-src 'unsafe-inline' 'unsafe-eval' blob: 'self'");
+  res.setHeader('Content-Security-Policy', "script-src 'unsafe-inline' 'unsafe-eval' blob: https://gitlab.igem.org");
   return res.status(200).send(content);
 }
